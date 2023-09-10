a46d54c897010e852b8ac5f263b9609168ab7d30
“Kreestee88” <christianaadakpo@gmail.com>
2023-09-09 19:40:00 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Smile in the mirror
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char b;
+
+	b = 'z';
+	while
+		(b >= 'a') {
+			putchar(b);
+			b--;
+		}
+			putchar('\n');
+			return (0);
+}
