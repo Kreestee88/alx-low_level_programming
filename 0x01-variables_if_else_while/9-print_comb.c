804d6ddfdc77441cc4286bd2afcbd30d91c44d06
“Kreestee88” <christianaadakpo@gmail.com>
2023-09-09 20:34:09 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Patience, persistence and perspiration make an unbeatable combinatio
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+	int p;
+
+	n = 'a';
+	p = 0;
+	while
+		(p < 10) {
+			putchar(p + '0');
+			p++;
+		}
+	while
+		(n <= 'f') {
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
