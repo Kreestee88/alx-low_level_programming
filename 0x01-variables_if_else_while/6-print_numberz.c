779649ae98645df044e0ed546915b5801d54e56c
“Kreestee88” <christianaadakpo@gmail.com>
2023-09-09 19:31:03 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program that prints all single digit numbers of base 10 starting fro
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int g;
+
+	g = 0;
+	while
+		(g < 10) {
+			putchar(g + '0');
+			g++;
+		}
+	putchar('\n');
+	return (0);
+}
