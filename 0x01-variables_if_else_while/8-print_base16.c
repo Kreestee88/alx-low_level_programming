cf2ff9ce88c8b3abea1ac5e08e587514c0d34543
“Kreestee88” <christianaadakpo@gmail.com>
2023-09-09 20:07:20 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Hexadecimal
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char f;
+
+	int g;
+
+	f = 'a';
+	g = 0;
+	while
+		(g < 10) {
+			putchar(g + '0');
+					g++;
+					}
+					while
+					(f <= 'h') {
+					putchar(f);
+					f++;
+					}
+					putchar('\n');
+					return (0);
+}
