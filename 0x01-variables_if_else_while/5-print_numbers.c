6532568bfea0482cce8cda199645456cd0c4afb3
“Kreestee88” <christianaadakpo@gmail.com>
2023-09-09 18:55:16 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program that prints all singledigit numbers of base 10 starting from0
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	putchar('\n');
+	return (0);
+}
