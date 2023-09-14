804a8f6d1c98384252e63080e509a263388d09da
“Kreestee88” <christianaadakpo@gmail.com>
2023-09-14 01:07:41 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  _isuper - a function that checks for uppercase character.
+ * @c: Variable
+ * Return: 0 (Success)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
