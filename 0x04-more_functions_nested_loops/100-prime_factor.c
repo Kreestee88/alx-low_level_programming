8469532713b54a633964d0ce96f500d69c4c6f76
“Kreestee88” <christianaadakpo@gmail.com>
2023-09-14 21:47:38 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
+
+	for (i = 3; i < 782848; i = i + 2)
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
